a75bcd5f547d140785626256985560c7d58a66dc
peter_todd <ptr.tdd@gmail.com>
2020-01-10 10:30:39 +0000

    comment about ts

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //related: https://repl.it/repls/AccurateEnviousCables
+
 var log; //presumed assigned as a side-effect in some other file.
+
 //https://www.youtube.com/watch?v=6apMqv4k1Iw
 log("HELLLLLOOOOOOOOO???"); //the argument is stripped from build output.
+//Note that typescript with 'declare var' does not exhibit the same issue.
